ed4a51fa2490d0d99fb5d483f04adafb6b187104
tosaken1116 <nagato20021122@gmail.com>
2023-01-18 11:56:12 +0900

    add:apolloClientの実装

---
+++ libs/apolloClient.ts
@@ -0,0 +1,28 @@
+import {
+  ApolloClient,
+  HttpLink,
+  InMemoryCache,
+  NormalizedCacheObject
+} from "@apollo/client";
+import "cross-fetch/polyfill";
+
+  let apolloClient: ApolloClient<NormalizedCacheObject> | undefined;
+const createApolloClient = () => {
+    return new ApolloClient({
+      ssrMode: typeof window === "undefined",
+      link: new HttpLink({
+        uri: "https://flowing-squid-27.hasura.app/v1/graphql",
+        headers: {
+          "x-hasura-admin-secret": process.env.NEXT_PUBLIC_HASURA_GRAPHQL_ADMIN_SECRET,
+        },
+      }),
+      cache: new InMemoryCache(),
+    });
+  };
+  export const initializeApollo = (initialState = null) => {
+    const _apolloClient = apolloClient ?? createApolloClient();
+    if (typeof window === "undefined") return _apolloClient;
+    if (!apolloClient) apolloClient = _apolloClient;
+
+    return _apolloClient;
+  };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ApolloProvider } from "@apollo/client";
+import type { AppProps } from "next/app";
+import { initializeApollo } from "../libs/apolloClient";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    const client = initializeApollo();
+    return (
+        <ApolloProvider client={client}>
+            <Component {...pageProps} />
+        </ApolloProvider>
+    );
 }
 
-export default MyApp
+export default MyApp;
