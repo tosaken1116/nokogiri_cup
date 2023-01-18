47e00dde8cb2c3cb454cb4f4593c2e04d265b5a1
tosaken1116 <nagato20021122@gmail.com>
2023-01-18 13:07:08 +0900

    add:codegenの実装

---
+++ codegen.ts
@@ -0,0 +1,19 @@
+
+import type { CodegenConfig } from '@graphql-codegen/cli';
+const config: CodegenConfig = {
+  overwrite: true,
+  schema: [{
+    "https://flowing-squid-27.hasura.app/v1/graphql": {
+      headers: {
+        'x-hasura-admin-secret':String(process.env.HASURA_GRAPHQL_ADMIN_SECRET)
+  }}}],
+  documents: "pages/**/*.tsx",
+  generates: {
+    "src/gql/": {
+      preset: "client",
+      plugins: []
+    }
+  }
+};
+
+export default config;
