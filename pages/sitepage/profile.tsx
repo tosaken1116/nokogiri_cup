786b7b0a89889c844c00eba856e9716d316afef1
naka520 <yashipipi0520@gmail.com>
2023-01-20 19:01:20 +0900

    "pageadd"

--- pages/sitepage/profile.tsx
+++ pages/sitepage/profile.tsx
@@ -1,1 +1,34 @@
 import type { NextPage } from "next";
+import Link from "next/link";
+import styles from "../../styles/Home.module.css";
+
+const ProductsList = () => {
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <h2>できた嬉しい！</h2>
+        <h2 className={styles.title}>商品一覧</h2>
+
+        <ul>
+          <li>
+            <Link href="/products/smartPhone">
+              <a>スマートフォン</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/products/pc">
+              <a>パソコン</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/products/headPhone">
+              <a>ヘッドホン</a>
+            </Link>
+          </li>
+        </ul>
+      </main>
+    </div>
+  );
+};
+
+export default ProductsList;
