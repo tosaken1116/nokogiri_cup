2b140ae47f9d5d3b213838fef770400a60297b6a
tosaken1116 <nagato20021122@gmail.com>
2023-01-21 23:20:09 +0900

    add:プロフィールのポップオーバーを作成 #30

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,13 @@ import NowPage from "./components/NowPage";
 import Sidebar from "./components/sidebar";
 export default function Home() {
     const [nowPage, setNowPage] = useState("home");
+    // const [anchorElement, setAnchorElement] =
+    //     useState<HTMLButtonElement | null>(null);
+    // const handleOpenProfilePopOver = (
+    //     event: React.MouseEvent<HTMLButtonElement>
+    // ) => {
+    //     setAnchorElement(event.currentTarget);
+    // };
     return (
         <Box>
             <Stack>
@@ -13,6 +20,11 @@ export default function Home() {
                     <Sidebar nowPage={nowPage} setNowPage={setNowPage} />
                     <NowPage nowPage={nowPage}></NowPage>
                 </Stack>
+                {/* <Button onClick={handleOpenProfilePopOver}>test</Button>
+                <ProfilePopOver
+                    anchorElement={anchorElement}
+                    closePopOver={() => setAnchorElement(null)}
+                /> */}
             </Stack>
         </Box>
     );
