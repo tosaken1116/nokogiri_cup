eb0820ac5a9d8126c237dfce421badfac7660031
naka520 <yashipipi0520@gmail.com>
2023-01-21 17:53:09 +0900

    [fix] リスト部以外のレイアウト整理

--- pages/notice.tsx
+++ pages/notice.tsx
@@ -8,7 +8,21 @@ import Typography from "@mui/material/Typography";
 import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
 import { ApolloProvider } from "@apollo/client";
-import SvgIcon from "@mui/material/SvgIcon";
+
+import { useContext, useState, useRef, useEffect } from "react";
+
+// eslint-disable-next-line react-hooks/rules-of-hooks
+const companies = ["Google", "Mozilla", "Microsoft"];
+const browsers: { name: string; url: string }[] = [
+  { name: "Chrome", url: "https://chromeenterprise.google" },
+  { name: "Firefox", url: "https://www.mozilla.org/ja/firefox/new/" },
+  { name: "Edge", url: "https://www.microsoft.com/ja-jp/edge" },
+];
+
+const listItems = companies.map((companied) => (
+  // eslint-disable-next-line react/jsx-key
+  <li>{companied}</li>
+));
 
 const NotionPage = () => {
   return (
@@ -18,6 +32,7 @@ const NotionPage = () => {
         <Box component="span" sx={{ width: 1, bgcolor: "#EEEEEE" }}>
           <Typography
             align="center"
+            variant="h5"
             sx={{
               color: "#333333",
               bgcolor: "#DDDDDD",
@@ -27,12 +42,11 @@ const NotionPage = () => {
             Notice All
           </Typography>
         </Box>
-        <Box
-          component="span"
-          sx={{ width: 1, height: 500, bgcolor: "#EEEEEE" }}
-        >
-          aaaaaa
+        <Box sx={{ width: 1, height: 200, bgcolor: "white" }}>
+          <Typography variant="h6"></Typography>
         </Box>
+
+        <ul>{listItems}</ul>
       </main>
     </div>
   );
