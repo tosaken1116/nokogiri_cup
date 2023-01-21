8c56938e609911a6e66e9066034956a5e4607aca
ray03j <eihouse.potato@outlook.jp>
2023-01-21 19:43:48 +0900

    [add] ポートフォリオ画面の追加-#17

---
+++ pages/Portfolio.tsx
@@ -0,0 +1,13 @@
+import { Box } from "@mui/material";
+import Article from "./components/Article";
+import Header from "./components/head";
+
+export default function Portfolio() {
+  
+  return (
+    <Box>
+    <Header />
+    <Article />
+    </Box>
+  )
+}
