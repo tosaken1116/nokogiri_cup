ac19f7dff16242ab1f773d93d0e35c25bf2b9afb
naka520 <yashipipi0520@gmail.com>
2023-01-21 12:20:51 +0900

    "サインイン・アウト時の変異を追加"

--- pages/components/head.tsx
+++ pages/components/head.tsx
@@ -2,16 +2,41 @@ import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
 
+const status = 1;
 export default function Header() {
   return (
-    <Box sx={{ flexGrow: 1, color: "text.disabled" }}>
-      <AppBar position="static" sx={"background-color:#EEEEEE;"}>
-        <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Surportforio仮
-          </Typography>
-        </Toolbar>
+    <Box sx={{ flex: "auto", color: "text.disabled" }}>
+      <AppBar position="static" sx={"background-color:#EEEEEE"}>
+        <Stack direction="row">
+          <Toolbar>
+            <Typography
+              variant="h6"
+              component="div"
+              sx={{ flexGrow: 1, color: "black" }}
+            >
+              Surportforio仮
+            </Typography>
+          </Toolbar>
+          {status ? (
+            <Box>
+              <Stack direction="row">
+                <Button variant="contained" disabled>
+                  singin
+                </Button>
+                <Button variant="contained" disabled>
+                  signup
+                </Button>
+              </Stack>
+            </Box>
+          ) : (
+            <Button variant="contained" disabled>
+              Username
+            </Button>
+          )}
+        </Stack>
       </AppBar>
     </Box>
   );
