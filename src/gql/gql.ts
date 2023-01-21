ea887d8af2e41b51b04c6bf450d1db5b060bd066
tosaken1116 <nagato20021122@gmail.com>
2023-01-21 18:44:34 +0900

    add:ユーザー認証の実装 #2

--- src/gql/gql.ts
+++ src/gql/gql.ts
@@ -13,7 +13,7 @@ import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/
  * Therefore it is highly recommended to use the babel-plugin for production.
  */
 const documents = {
-    "\n    query testquery {\n      user {\n        userName\n        userMail\n      }\n    }\n  ": types.TestqueryDocument,
+    "\n        query getArticle {\n            article {\n                authorId\n                id\n                createdAt\n                title\n                caption\n            }\n        }\n    ": types.GetArticleDocument,
 };
 
 /**
@@ -33,7 +33,7 @@ export function graphql(source: string): unknown;
 /**
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
-export function graphql(source: "\n    query testquery {\n      user {\n        userName\n        userMail\n      }\n    }\n  "): (typeof documents)["\n    query testquery {\n      user {\n        userName\n        userMail\n      }\n    }\n  "];
+export function graphql(source: "\n        query getArticle {\n            article {\n                authorId\n                id\n                createdAt\n                title\n                caption\n            }\n        }\n    "): (typeof documents)["\n        query getArticle {\n            article {\n                authorId\n                id\n                createdAt\n                title\n                caption\n            }\n        }\n    "];
 
 export function graphql(source: string) {
   return (documents as any)[source] ?? {};
