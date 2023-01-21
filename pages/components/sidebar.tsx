b91336b53847854fd2cd18ce78ba5426a8b59762
MitsukuniH <hmitsuku3292@gmail.com>
2023-01-21 20:06:00 +0900

    [fix]サイドバーの修正２

--- pages/components/sidebar.tsx
+++ pages/components/sidebar.tsx
@@ -1,24 +1,22 @@
 import styled from '@emotion/styled'
-import { Button, Stack } from "@mui/material"
+import { Button, IconButton, Paper, Stack } from "@mui/material"
 
-const IconBtn = styled(Button)`
+const SideIconButton = styled(IconButton)`
     width: 64px;
     height: 64px;
-    border-radius: 50%;
     margin: 6px;
     background: #ddd;
-    :hover{
-        background: #ccc;
-    }
 `;
 export const Sidebar = ()=>{
     return(
-        <Stack boxShadow={5} sx={{ zIndex: 1300, minHeight: "100vh", width: "fit-content" ,border: "2", padding: "2px"}}>
-            <IconBtn variant='contained'/>
-            <IconBtn variant='contained'/>
-            <IconBtn variant='contained'/>
-            <IconBtn variant='contained'/>
-            <IconBtn variant='contained'/>
+        <Paper>
+        <Stack alignItems="center" spacing={1.5} boxShadow={5} sx={{ zIndex: 1300, minHeight: "100vh", width: "fit-content" ,border: "2", padding: "2px"}}>
+            <SideIconButton/>
+            <SideIconButton/>
+            <SideIconButton/>
+            <SideIconButton/>
+            <SideIconButton/>
         </Stack>
+        </Paper>
     )
 }
