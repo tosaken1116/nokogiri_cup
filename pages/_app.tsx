a2b898a614e20e863165fd85d3220290588b640b
tosaken1116 <nagato20021122@gmail.com>
2023-01-25 01:34:21 +0900

    add:ログイン状態による権限の変更を実装

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
 import { ApolloProvider } from "@apollo/client";
 import type { AppProps } from "next/app";
+import { useLocalStorage } from "../Hooks/hooks";
 import { initializeApollo } from "../libs/apolloClient";
 import "../styles/globals.css";
 import "../styles/markdown.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-    const client = initializeApollo();
+    const { getLocalStorage } = useLocalStorage();
+    const client = initializeApollo(getLocalStorage("authToken"));
     return (
         <ApolloProvider client={client}>
             <Component {...pageProps} />
