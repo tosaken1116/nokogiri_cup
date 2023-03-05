1591e8438fffd1d3da44c69b0391d771c985b2da
tosaken1116 <nagato20021122@gmail.com>
2023-03-05 23:05:28 +0900

    add: google fontsの追加 #90

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import { ApolloProvider } from "@apollo/client";
 import { ThemeProvider } from "@mui/material";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import Layout from "../components/Layout/Layout";
 import { createMuiTheme } from "../Hooks/createTheme";
 import { useLocalStorage } from "../Hooks/hooks";
@@ -14,6 +15,18 @@ function MyApp({ Component, pageProps }: AppProps) {
     const theme = createMuiTheme();
     return (
         <ApolloProvider client={client}>
+            <Head>
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link
+                    rel="preconnect"
+                    href="https://fonts.gstatic.com"
+                    crossOrigin="true"
+                />
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Yusei+Magic&display=swap"
+                    rel="stylesheet"
+                />
+            </Head>
             <ThemeProvider theme={theme}>
                 <Layout>
                     <Component {...pageProps} />
